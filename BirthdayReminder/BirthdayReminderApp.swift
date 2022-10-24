b11e595c7faf1aecaf35ce3472fd70cb39062a6f
Ruslan <Alekyan_2016@mail.ru>
2022-10-24 14:21:47 +0300

    Initial Commit

---
+++ BirthdayReminder/BirthdayReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayReminderApp.swift
+//  BirthdayReminder
+//
+//  Created by Ruslan Alekyan on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
